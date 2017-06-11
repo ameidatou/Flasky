a34274c53c2dfae540aa85693cedfd6d8f4e58b2
ameidatou <imed.mnif@gmail.com>
2017-06-11 13:54:13 +0100

    Database integration with SQLAlchemy'

--- hello.py
+++ hello.py
@@ -1,13 +1,26 @@
+import os
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
+from flask_script import Manager
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+manager = Manager(app)
+
 app.config['SECRET_KEY'] = 'flask web dev secret key'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
@@ -17,13 +30,20 @@ def index():
     name = None
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session['name']
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
         form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
+    return render_template('index.html',
+        form=form, name=session.get('name'),
+        known=session.get('known', False),
+        current_time=datetime.utcnow())
 
     #return render_template('index.html', current_time=datetime.utcnow())
     #user_agent = request.headers.get('User-Agent')
@@ -47,5 +67,25 @@ class NameForm(FlaskForm):
     name = StringField('What is you name?', validators=[Required()])
     submit = SubmitField('Submit')
 
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
+    #app.run(debug=True)
