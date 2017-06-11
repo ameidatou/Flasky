8472739ba8bc85a919fa0ba017dfc50f50854874
ameidatou <imed.mnif@gmail.com>
2017-06-11 14:15:55 +0100

    Add migrations

--- hello.py
+++ hello.py
@@ -8,6 +8,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -21,6 +22,8 @@ app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
